e86d9be6cca3e7864c263b21678cc063975fec85
locky <inox127@hotmail.com>
2018-10-07 02:51:54 +0200

    added alt files

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function guessLetter() {
             } else {
                 faux = true;
             }
-        }
+       
         if(vrai !== true && faux === true) {
             alert('RATÉ');
             echec++;
@@ -48,7 +48,7 @@ function guessLetter() {
     while (verifTab.join() !== tab.join()) {
         let lettre = prompt(`DEVINEZ LE MOT SECRET : ${verifTab}\nVous avez entré : ${memory}`).toUpperCase();
         while (lettre.length > 1) {
-            alert('ENTREZ UN SEUL CARACTÉRE !');
+            alert('ENTREZ UN SEUL CARACTERE !');
             return guessLetter();
         }
         let vrai;
@@ -66,9 +66,30 @@ function guessLetter() {
         if (vrai !== true && faux === true) {
             alert('RATÉ');
             echec++;
+            
+            if (echec === 1) {
+                document.querySelectorAll(".pendu")[0].className = "pendu hang2";
+            } else if (echec === 2) {
+                document.querySelectorAll(".pendu")[0].className = "pendu hang3";
+            } else if (echec === 3) {
+                document.querySelectorAll(".pendu")[0].className = "pendu hang4";
+            } else if (echec === 4) {
+                document.querySelectorAll(".pendu")[0].className = "pendu hang5";
+            } else if (echec === 5) {
+                document.querySelectorAll(".pendu")[0].className = "pendu hang6";
+            } else if (echec >= 6) {
+                document.querySelector(".pendu").className = "pendu hang7";
+                break;
+            } 
         }
     }
-    confirm(`FÉLICITATION ! VOUS AVEZ REUSSI AVEC ${echec} FAUTE(S)`);
+    
+    if (echec >= 6) {
+        document.querySelector(".guess").innerHTML = `<p>HELAS, VOUS N'AVEZ GUERE PU LE SAUVER...</p>`;
+    } else if (verifTab.join() === tab.join()) {
+        document.querySelector(".guess").innerHTML = `<p>FÉLICITATION ! VOUS AVEZ REUSSI AVEC ${echec} FAUTE(S)</p>`;
+    }
+    
 }
 
 guessLetter();
